7853c9a33ca44d762b0adb1ec3243ecb558da94b
Sanul-Raskar <sanulraskar@gmail.com>
2019-06-13 18:28:11 +0530

    Added home banner

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,2 +1,18 @@
 console.log("JS Connected");
 sal();
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 300) {
+        $(".topButton").addClass("buttonDisplay");
+      } else {
+        $(".topButton").removeClass("buttonDisplay");
+      }
+  
+      if ($(this).scrollTop() > 250) {
+        $(".navbar").addClass("solid");
+        $(".navbar").css("background-color: #000;");
+      } else {
+        $(".navbar").removeClass("solid");
+      }
+    });
+  });
